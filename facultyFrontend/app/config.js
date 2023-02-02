15a884fcb1386b1386da8d6e2636d352a499a0c3
Ayush Baliyan <ayush.3316403221@ipu.ac.in>
2023-02-03 00:15:42 +0530

    backend changed

--- facultyFrontend/app/config.js
+++ facultyFrontend/app/config.js
@@ -1,5 +1,5 @@
 //checking 26072021_21:33
-// var BACKEND = 'https://sfs2022.herokuapp.com';
-var BACKEND = 'http://localhost:8080';
+var BACKEND = 'https://sfs.onrender.com';
+// var BACKEND = 'http://localhost:8080';
 var CURR_YEAR=2022;
 var ODD_EVEN=1;
